27a4464e5d56682973776d098b171789bc976d8d
Real-Bird <marbil815@gmail.com>
2022-03-18 23:19:00 +0900

    22-03-18 modified background

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,15 +1,14 @@
-const BG_IMG = "bgImg"
+const BG_IMG = "bgImg";
 
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-];
+const images = [];
+
+for (let i = 0; i < 7; i++) {
+  images.push(`${i}.jpg`);
+}
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.classList.add(BG_IMG);
 bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage); // body 맨 끝에 생성
-// document.body.prepend(bgImage); // body 맨 위에 생성
+document.body.appendChild(bgImage);
